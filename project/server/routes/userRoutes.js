bae802f80d4410493bdb8b7dcceeb9b0d65b58ac
mrinal1224 <iammrinalkc@gmail.com>
2024-02-20 21:59:37 +0530

    login route

--- project/server/routes/userRoutes.js
+++ project/server/routes/userRoutes.js
@@ -1,43 +1,70 @@
-const express = require('express')
-const User = require('../models/userModel')
-const bcrypt = require('bcrypt')
+const express = require("express");
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
 
-const router = express.Router()
+const router = express.Router();
+
+router.post("/register", async (req, res) => {
+  try {
+    const userExists = await User.findOne({ email: req.body.email });
+
+    if (userExists) {
+      res.send({
+        success: false,
+        message: "user already Exists",
+      });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+
+    console.log(salt);
+
+    const hashPwd = bcrypt.hashSync(req.body.password, salt);
+    req.body.password = hashPwd;
+
+    const newUser = await User(req.body);
+    await newUser.save();
+
+    res.send({
+      success: true,
+      message: "User Registered",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 
-
-router.post('/register' , async(req , res)=>{
+router.post('/login' , async(req , res)=>{
     try {
+        const user = await User.findOne({ email: req.body.email })
 
-        const userExists = await User.findOne({email:req.body.email})
-
-        if(userExists){
+        if(!user){
             res.send({
-                success : false,
-                message : "user already Exists"
+                success: false,
+                message: "user does not exist Please Register",
+              });
+        }
+
+        const validPassword = await bcrypt.compare(req.body.password , user.password)
+
+        if(!validPassword){
+            res.send({
+                success: false,
+                message: "Sorry, invalid password entered!"
             })
         }
 
-        const salt = await bcrypt.genSalt(10)
-
-        console.log(salt)
-
-        const hashPwd = bcrypt.hashSync(req.body.password , salt)
-        req.body.password = hashPwd
-
-
-
-        const newUser = await User(req.body)
-        await newUser.save()
-
         res.send({
-            success : true,
-            message : "User Registered"
-        })
+            success: true,
+            message: "You've successfully logged in!",
         
+        })
+
+
     } catch (error) {
-        console.log(error)
+         console.error(error)
     }
 })
 
-module.exports = router
+module.exports = router;
