cfaf4c8915a8c09c05cac28f07fcb3cdae66bb92
mrinal1224 <iammrinalkc@gmail.com>
2024-02-26 23:21:11 +0530

    push

--- project/client/src/components/ProtectedRoute.js
+++ project/client/src/components/ProtectedRoute.js
@@ -1,28 +1,42 @@
-import React, { useEffect , useState } from "react";
+import React, { useEffect, useState } from "react";
 import { GetCurrentUser } from "../calls/users";
 import { useNavigate } from "react-router-dom";
-import {message} from 'antd'
-import { useDispatch  , useSelector} from "react-redux";
+import { message, Layout , Menu } from "antd";
+import { useDispatch, useSelector } from "react-redux";
 import { hideLoading, showLoading } from "../redux/loaderSlice";
+import { Header } from "antd/es/layout/layout";
+import {HomeOutlined , UserOutlined} from "@ant-design/icons"
 
 function ProtectedRoute({ children }) {
-   
-   const [user , setUser] = useState(null)
-   const dispatch = useDispatch()
+
+  const navItems = [
+    {
+      label : "Home",
+      icon : <HomeOutlined />
+    },
+
+    {
+      label : "Profile",
+      icon : <UserOutlined />
+    },
+
+]
+
+  const [user, setUser] = useState(null);
+  const dispatch = useDispatch();
 
   // random checks
   const getValidUser = async () => {
     try {
-      dispatch(showLoading())
+      dispatch(showLoading());
       const response = await GetCurrentUser();
       console.log(response);
-      setUser(response.data)
-      dispatch(hideLoading())
-    // Hide Loader
+      setUser(response.data);
+      dispatch(hideLoading());
+      // Hide Loader
     } catch (error) {
-      setUser(null)
+      setUser(null);
       message.error(error.message);
-     
     }
   };
 
@@ -36,7 +50,27 @@ function ProtectedRoute({ children }) {
     }
   }, []);
 
-  return <div> {user && user.name}</div>;
+  return (
+    <>
+      <Layout>
+        <Header
+          className="d-flex justify-content-between"
+          style={{
+            position: "sticky",
+            top: 0,
+            zIndex: 1,
+            width: "100%",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <h3 className="demo-logo text-white m-0" style={{ color: "white" }}>Book My Show</h3>
+          <Menu theme='dark' mode='horizontal' items={navItems}/>
+
+        </Header>
+      </Layout>
+    </>
+  );
 }
 
 export default ProtectedRoute;
