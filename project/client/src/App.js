fa9059443766f618c32afce9d6f6dae1781cf2c0
mrinal1224 <iammrinalkc@gmail.com>
2024-02-27 22:06:25 +0530

    pushing

--- project/client/src/App.js
+++ project/client/src/App.js
@@ -6,20 +6,46 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ProtectedRoute from "./components/ProtectedRoute";
 import { useSelector } from "react-redux";
-
+import Profile from "./pages/Profile";
+import Admin from "./pages/Admin";
 
 function App() {
-
-
-  const {loading} = useSelector((state) => state.loader)
-  
+  const { loading } = useSelector((state) => state.loader);
 
   return (
     <div className="App">
-      {loading && <div className="loader-container"> <div className="loader">  </div> </div>}
+      {loading && (
+        <div className="loader-container">
+          {" "}
+          <div className="loader"> </div>{" "}
+        </div>
+      )}
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin"
+            element={
+              <ProtectedRoute>
+                <Admin />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
--- project/client/src/components/ProtectedRoute.js
+++ project/client/src/components/ProtectedRoute.js
@@ -1,59 +1,76 @@
 import React, { useEffect, useState } from "react";
 import { GetCurrentUser } from "../calls/users";
 import { useNavigate } from "react-router-dom";
-import { message, Layout , Menu } from "antd";
+import { message, Layout, Menu } from "antd";
 import { useDispatch, useSelector } from "react-redux";
 import { hideLoading, showLoading } from "../redux/loaderSlice";
 import { Header } from "antd/es/layout/layout";
-import {HomeOutlined , LogoutOutlined, ProfileOutlined, UserOutlined} from "@ant-design/icons"
-import {Link} from "react-router-dom"
+import {
+  HomeOutlined,
+  LogoutOutlined,
+  ProfileOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import { Link } from "react-router-dom";
+import { setUser } from "../redux/userSlice";
 
 function ProtectedRoute({ children }) {
+  const { user } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const navItems = [
     {
-      label : "Home",
-      icon : <HomeOutlined />
+      label: "Home",
+      icon: <HomeOutlined />,
     },
 
     {
-      label : "Profile",
-      icon : <UserOutlined />,
+      label: `${user ? user.name : ""}`,
+      icon: <UserOutlined />,
       children: [
         {
-          label : "My Profile",
-          icon : <ProfileOutlined/>
+          label: (
+            <span
+              onClick={() => {
+                user.isAdmin ? navigate("/admin") : navigate("/profile");
+              }}
+            >
+              My Profile
+            </span>
+          ),
+          icon: <ProfileOutlined />,
         },
 
         {
-          label : <Link to='/login' onClick={(()=> {localStorage.removeItem('token')})}>Log Out</Link>,
-          icon : <LogoutOutlined/>
-        }
-           
-      ]
+          label: (
+            <Link
+              to="/login"
+              onClick={() => {
+                localStorage.removeItem("token");
+              }}
+            >
+              Log Out
+            </Link>
+          ),
+          icon: <LogoutOutlined />,
+        },
+      ],
     },
+  ];
 
-]
-
-  const [user, setUser] = useState(null);
-  const dispatch = useDispatch();
-
-  // random checks
   const getValidUser = async () => {
     try {
       dispatch(showLoading());
       const response = await GetCurrentUser();
-      console.log(response);
-      setUser(response.data);
+      dispatch(setUser(response.data));
       dispatch(hideLoading());
       // Hide Loader
     } catch (error) {
-      setUser(null);
+      dispatch(setUser(null));
       message.error(error.message);
     }
   };
-
-  const navigate = useNavigate();
 
   useEffect(() => {
     if (localStorage.getItem("token")) {
@@ -64,25 +81,31 @@ function ProtectedRoute({ children }) {
   }, []);
 
   return (
-    <>
-      <Layout>
-        <Header
-          className="d-flex justify-content-between"
-          style={{
-            position: "sticky",
-            top: 0,
-            zIndex: 1,
-            width: "100%",
-            display: "flex",
-            alignItems: "center",
-          }}
-        >
-          <h3 className="demo-logo text-white m-0" style={{ color: "white" }}>Book My Show</h3>
-          <Menu theme='dark' mode='horizontal' items={navItems}/>
-
-        </Header>
-      </Layout>
-    </>
+    user && (
+      <>
+        <Layout>
+          <Header
+            className="d-flex justify-content-between"
+            style={{
+              position: "sticky",
+              top: 0,
+              zIndex: 1,
+              width: "100%",
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <h3 className="demo-logo text-white m-0" style={{ color: "white" }}>
+              Book My Show
+            </h3>
+            <Menu theme="dark" mode="horizontal" items={navItems} />
+          </Header>
+          <div style={{ padding: 24, minHeight: 380, background: "#fff" }}>
+            {children}
+          </div>
+        </Layout>
+      </>
+    )
   );
 }
 
